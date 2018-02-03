25aa3ef281d988cd7cfd695be038eccbcc2337a2
Maciej Lelusz <maciej.lelusz@inleo.pl>
2018-02-03 15:50:29 +0100

    Create twitterbot.py

---
+++ twitterbot.py
@@ -0,0 +1,32 @@
+import twitter
+import json
+import random
+
+with open('credentials.json') as file:
+    credentials = json.loads(file.read())
+
+api = twitter.Api(**credentials)
+
+def get_tweets():
+  track ='q=%23'
+  track += 'wine'
+  track += '-filter%3'
+  track += 'Alinks'
+  track += '&lang=en'
+  track += '&count=20'
+  
+  results = api.GetSearch(raw_query=track)
+  
+  return result
+
+def retweet(tweets):
+    if not tweets:
+        return False
+
+    to_retweet = max(tweets, key=lambda status: status.favorite_count + status.retweet_count)
+    
+    api.PostRetweet(status_to_retweet.id)
+
+def lambda_handler(_event_json, _context):
+  tweets = get_tweets()
+  retweet(tweets)
